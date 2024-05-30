e33b37b8185f930a25dab7f6717fb284eb07d15f
Tim <github@timgreen.ws>
2024-05-30 18:07:21 +1000

    init app

---
+++ index.ts
@@ -0,0 +1,32 @@
+import './styles.css';
+
+import { faker, fakerEN_AU } from '@faker-js/faker';
+
+const appDiv: HTMLElement = document.querySelector('#app');
+for (const faker of [fakerEN_AU]) {
+  const fullName = faker.person.fullName();
+  appDiv.innerHTML += `
+  <div class="container">
+      <div class="card">
+        <div class="card__image">
+          <img src="${faker.image.url()}" alt="Background image for ${fullName}"/>
+        </div>
+        <div class="card__profile">
+          <img src="${faker.image.avatar()}" alt="Avatar image of ${fullName}"/>
+        </div>
+        <div class="card__body">
+          <h2>${fullName}</h2>
+          <p class="fTitle">Address</p>
+          <p>${faker.location.streetAddress()}, ${faker.location.city()}, ${faker.location.state()}</p>
+          <p class="fTitle">Phone</p>
+          <p>${faker.phone.number()}</p>
+         
+          <p class="fTitle">Job Type</p>
+          <p>${faker.person.jobType()}</p>
+          <p class="fTitle">Job Title</p>
+          <p>${faker.person.jobTitle()}</p>
+        </div>  
+      </div>
+    </div>
+`;
+}
