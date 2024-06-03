f2e1ce0b49ecf9355bc7aa4cb8af28b9dadf166f
Tim <github@timgreen.ws>
2024-06-03 14:38:57 +1000

    added refresh button

--- index.ts
+++ index.ts
@@ -2,6 +2,15 @@ import './styles.css';
 
 import { faker, fakerEN_AU } from '@faker-js/faker';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.refresh') as HTMLButtonElement;
+  button.addEventListener('click', refreshPage);
+
+  function refreshPage() {
+      location.reload();
+  }
+});
+
 const appDiv: HTMLElement = document.querySelector('#app');
 for (const faker of [fakerEN_AU]) {
   const fullName = faker.person.fullName();
@@ -13,6 +22,7 @@ for (const faker of [fakerEN_AU]) {
         </div>
         <div class="card__profile">
           <img src="${faker.image.avatar()}" alt="Avatar image of ${fullName}"/>
+         
         </div>
         <div class="card__body">
           <h2>${faker.person.firstName()} ${faker.person.lastName()}</h2>
@@ -24,7 +34,10 @@ for (const faker of [fakerEN_AU]) {
           <p>${faker.person.jobType()}</p>
           <p class="fTitle">Job Title</p>
           <p>${faker.person.jobTitle()}</p>
+
+          <button class="button refresh">Refresh Data</button>
         </div>  
+        
       </div>
     </div>
 `;
